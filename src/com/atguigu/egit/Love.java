d4912828447e969c6283c997ab7afe6f332502dd
suqmfu2 <962473054@qq.com>
2020-02-25 19:54:15 +0800

    su2写入给su1pull==

--- src/com/atguigu/egit/Love.java
+++ src/com/atguigu/egit/Love.java
@@ -5,6 +5,7 @@ public class Love {
 	public static void main(String[] args) {
 		System.out.println("su1第一次写入======");
 		System.out.println("su2第一次写入------");
+		System.out.println("su2写入给su1pull==");
 	}
 
 }
